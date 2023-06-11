958b51bbd3a0a4f5f177d84f57328752eff6d5f5
yeoni sugar <rkdgkdus522@naver.com>
2023-06-11 22:30:22 +0900

    [BUGFIX] : eslint bug 수정

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,3 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
