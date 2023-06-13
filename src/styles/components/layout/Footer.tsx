531279f78737a684f63140760d08cdaea3f99af9
Jeong-ki <7863z@naver.com>
2023-06-13 23:03:10 +0900

    [ADD] : footer 문구 크기 변경

--- src/styles/components/layout/Footer.tsx
+++ src/styles/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const FooterWrapper = styled.footer`
   margin-top: auto;
   padding: 60px;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 400;
   line-height: 1.5;
   color: #bdbcbc;
