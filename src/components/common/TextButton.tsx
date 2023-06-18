add7145aabf718793121caf3067f0ef7ee77ac42
yeoni sugar <rkdgkdus522@naver.com>
2023-06-18 11:41:02 +0900

    [UPDATE] : TextButton opacity, border style add & code refactoring

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -10,17 +10,23 @@ interface ButtonProps {
   width?: number | undefined;
   height?: number | undefined;
   weight?: number | undefined;
+  border?: boolean | undefined;
   onClick: React.MouseEventHandler<HTMLElement>;
 }
 
 const BtnWrapper = styled.button<ButtonProps>`
-  font-size: ${(props) => `${props.fontSize || 20}px`};
-  font-weight: ${(props) => props.weight || 600};
-  width: ${(props) => `${props.width || 300}px`};
-  height: ${(props) => `${props.height || 57}px`};
-  color: ${(props) => props.color || "#fff"};
-  background-color: ${(props) => props.background || "#0d3471"};
+  font-size: ${({ fontSize }) => `${fontSize || 20}px`};
+  font-weight: ${({ weight }) => weight || 600};
+  width: ${({ width }) => `${width || 300}px`};
+  height: ${({ height }) => `${height || 57}px`};
+  color: ${({ color }) => color || "#fff"};
+  background-color: ${({ background }) => background || "#0d3471"};
+  border: ${({ border }) => (border ? "1px solid #808080;" : "0 none;")};
   border-radius: 10px;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const TextButton: React.FC<ButtonProps> = ({
