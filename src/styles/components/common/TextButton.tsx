e12b2fc78f1ba790110562b95f99bb7beb7f5f6a
Jeong-ki <7863z@naver.com>
2023-07-02 20:47:27 +0900

    [ADD] : 마이페이지 내 모임, 신청한 모임 마크업

--- src/styles/components/common/TextButton.tsx
+++ src/styles/components/common/TextButton.tsx
@@ -9,12 +9,8 @@ const BtnWrapper = styled.button<ButtonProps>`
   color: ${({ color }) => color || "#fff"};
   background-color: ${({ background }) => background || "#0d3471"};
   border: ${({ border }) => (border ? "1px solid #808080;" : "0 none;")};
-<<<<<<< HEAD
   border-radius: 5px;
-=======
-  border-radius: 10px;
   margin: ${({ margin }) => margin || "0"};
->>>>>>> bce97e4 ([UPDATE] : 로그인, 회원가입 버튼 TextButton 컴포넌트 적용)
 
   &:hover {
     color: ${({ hovercolor }) => hovercolor || "none"};
