f3d47b08b51f95e13adce9a21905befd1dc2fd11
yeoni sugar <rkdgkdus522@naver.com>
2023-07-05 23:47:34 +0900

    [ADD] : 채팅 목록 마크업 구현

---
+++ src/styles/components/chat/ChatRoomListAside.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+const ChatRoomListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 28rem;
+  height: 100%;
+  min-height: 800px;
+  box-shadow: 0.25rem 0 0.25rem rgba(0, 0, 0, 0.25);
+  gap: 1rem;
+  background-color: #ecf2ff;
+`;
+
+const ChatRoomList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  max-height: 60rem;
+  overflow-y: scroll;
+  gap: 0.1rem;
+
+  &::-webkit-scrollbar {
+    width: 7px;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    margin: 2px;
+  }
+`;
+
+const ChatRoomItem = styled.li`
+  background-color: #dee3ea;
+  padding: 20px 30px;
+
+  &.checked {
+    background-color: #bdc8d6;
+  }
+`;
+
+const ChatRoomItemTitle = styled.div`
+  font-size: 1.1rem;
+  font-weight: 600;
+  letter-spacing: 0.25px;
+  margin-bottom: 10px;
+  color: #0d3471;
+`;
+
+const ChatRoomItemContent = styled.div`
+  display: flex;
+  align-items: center;
+  overflow-x: hidden;
+  margin-bottom: 10px;
+`;
+
+const ChatRoomItemText = styled.div`
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 20rem;
+  font-size: 0.9rem;
+  letter-spacing: 0.25px;
+  color: #0d3471;
+`;
+
+const ChatRoomItemDate = styled.div`
+  margin-left: 15px;
+  font-size: 0.8rem;
+  font-weight: 600;
+  color: #0d3471;
+`;
+
+export {
+  ChatRoomListWrapper,
+  ChatRoomList,
+  ChatRoomItem,
+  ChatRoomItemTitle,
+  ChatRoomItemContent,
+  ChatRoomItemText,
+  ChatRoomItemDate,
+};
---
+++ src/utils/mock/chat.ts
@@ -0,0 +1,52 @@
+export const chatRoomList = [
+  {
+    chatRoomId: 1,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 2,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 3,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 4,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 5,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 6,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 7,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 8,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 9,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+  {
+    chatRoomId: 10,
+    chatRoomName: "만두만둘",
+    latestMessage: "우리 언제 만나야할까요? 다른 분들은 언제가 편하신가요?",
+  },
+];
