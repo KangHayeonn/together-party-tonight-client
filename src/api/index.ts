20266b7a3ebb035038272d95b5823fb99b5f0a8c
yeoni sugar <rkdgkdus522@naver.com>
2023-07-15 01:52:59 +0900

    [UPDATE] : KAKAO REST API KEY 환경변수 .env 파일로 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ const createInstanceKakao = () => {
     baseURL: "https://dapi.kakao.com",
     headers: {
       "Content-Type": "application/json; charset=utf-8",
-      Authorization: "KakaoAK ab965e9a61bf134a72b458f79290dc17",
+      Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
     },
   });
 };
